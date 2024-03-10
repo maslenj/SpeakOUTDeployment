3f691dfe72860c1450f51c3cf5ab6c558b29636e
maslenj <james.maslen@tufts.edu>
2024-03-10 10:17:28 -0400

    protect notification route

--- app/api/notifications/userId/route.ts
+++ app/api/notifications/userId/route.ts
@@ -1,13 +1,16 @@
+import { getSelf } from "@/lib/db/utils";
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 //API route to fetch all notifications for a specific user from the database
 export async function GET(request: Request) {
-    const { searchParams } = new URL(request.url)
-    const userID = searchParams.get('userId')
-
+    const user = await getSelf()
+    if (user == null) {
+        return new NextResponse(null, { status: 401 });
+    }
+    
     const notifications = await prisma.notification.findMany({
-        where: { userId: Number(userID) }
+        where: { userId: Number(user?.id) }
     })
 
     return NextResponse.json(notifications)
